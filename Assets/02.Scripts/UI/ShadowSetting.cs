41c90f1abe8a6c2e55a7559850d24d1c400e7745
tube0330 <zien0516@naver.com>
2024-09-02 14:58:14 +0900

    240902

--- Assets/02.Scripts/UI/ShadowSetting.cs
+++ Assets/02.Scripts/UI/ShadowSetting.cs
@@ -5,13 +5,32 @@ using UnityEngine.UI;
 
 public class ShadowSetting : MonoBehaviour
 {
+    public Dropdown dropdown;
+    public new Light light;
+
     void Start()
     {
-
+        dropdown.onValueChanged.AddListener(OnShadowDropValueChanged);
     }
 
-    void Update()
+    public void OnShadowDropValueChanged(int value)
     {
+        if (dropdown != null && light != null)
+        {
+            switch (value)
+            {
+                case 0:
+                    light.shadows = LightShadows.None;
+                    break;
+                case 1:
+                    light.shadows = LightShadows.Soft;
+                    break;
+                case 2:
+                    light.shadows = LightShadows.Hard;
+                    break;
+            }
+        }
 
+        else Debug.LogError("Directional Light와 Dropdown 중 하나가 없음");
     }
 }
--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 조이스틱으로 3D 캐릭터 조종하기 버튼으로 스킬동작 콤보 어택동작 대시하면서 어택 동작 만들기
 
 # 240902
-1. Pause, Sound UI 만들기
-2. 
+1. Pause, Sound, Screen UI 만들기
+2. Sound 조절
+- 배경음악
+- Mute
+- Player Skill Sound 조절
+3. Screen Setting
+- FOV 조절
+- Shadow 설정
